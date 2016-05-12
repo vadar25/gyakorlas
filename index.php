421e02057577c46c67886a2c259f725747f40abb
vadar <szekelyferenc25@gmail.com>
2016-05-12 11:10:34 +0200

    Working with files
    reading

--- index.php
+++ index.php
@@ -6,10 +6,30 @@ include "functions.php";
 /*then lets call our first function*/
 
 addingvalues(10,10);
+
+
 echo "<br>";
+
+/* writing to a file 
 $file = fopen('test.txt','w');
 fwrite($file,"Ferenc" . "\n");
-fwrite($file,"Tamás");
+fwrite($file,"Tamas");
 fclose($file);
+*/
 
+
+/*read the file line by line and then echo them out with foreach*/
+
+$readin = file("test.txt");
+$readin_count = count($readin);
+$count = 1;
+echo "These are the names so far: ";
+foreach ($readin as $names) {
+	if ($count == $readin_count){
+		echo $names . ".";
+	} else {
+		echo trim($names) . ", ";
+	}
+	$count++;
+}
 ?>
